8b75fb765b83e2805628fb3858c1bca28b04ae24
Jun Hao <e0012616@u.nus.edu>
2017-09-17 23:08:44 +0800

    Cleanup

--- Neural network models (supervised).py
+++ Neural network models (supervised).py
@@ -5,6 +5,60 @@ Created on Sun Sep 17 18:57:42 2017
 @author: Jun Hao
 """
 
+import csv
+import numpy as np 
+
+#Sort data into columns -> For some reason can't extract class column using the better(next) method
+with open('dataset-har-PUC-Rio-ugulino.csv') as csvfile:
+    reader=csv.reader(csvfile,delimiter=';')
+    headers = next(reader)
+    column = {}
+    for h in headers:
+        column[h] = []
+    for row in reader:
+        for h, v in zip(headers, row):
+            column[h].append(v)
+
+#Sort data into a whole array and extract necessary data
+testdata = np.genfromtxt ('dataset-har-PUC-Rio-ugulino.csv', delimiter=";")
+testdata = np.delete(testdata, (0), axis=0)
+X = testdata[:,[6,7,8,9,10,11,12,13,14,15,16,17]]
+
+
+#Data pre-processing
+from sklearn import preprocessing
+
+##Encode output variable  
+le = preprocessing.LabelEncoder()
+le.fit(['sittingdown', 'standingup', 'standing', 'walking', 'sitting'])
+#print(list(le.classes_))
+y = []
+y = le.transform(column['class'])
+
+#Normalize data
+normalized_X = preprocessing.normalize(X) 
+
+
+#Evaluate model
+from sklearn.svm import SVC
+from sklearn.model_selection import KFold
+from sklearn.metrics import confusion_matrix
+
+kfold = KFold(n_splits=10, shuffle=True)
+
+fold_index = 0
+for train, test in kfold.split(normalized_X):
+    svm_model_linear = SVC(kernel = 'linear', C = 1).fit(normalized_X[train], y[train])
+    svm_predictions = svm_model_linear.predict(normalized_X[test])
+    accuracy = svm_model_linear.score(normalized_X[test], y[test])
+    cm = confusion_matrix(y[test], svm_predictions)
+    print('In the %i fold, the classification accuracy is %f' %(fold_index, accuracy))
+    print('And the confusion matrix is: ')
+    print(cm)
+    fold_index +=1
+    
+    
+    
 #Neural Network classifier
 #from sklearn.neural_network import MLPClassifier
 #X = [[0., 0.], [1., 1.]]
@@ -28,52 +82,3 @@ Created on Sun Sep 17 18:57:42 2017
 #print(clf.predict([[1., 2.]]))
 # 
 #print(clf.predict([[0., 0.]]))
-
-import csv
-import numpy as np 
-
-
-with open('dataset-har-PUC-Rio-ugulino.csv') as csvfile:
-    reader=csv.reader(csvfile,delimiter=';')
-    headers = next(reader)
-    column = {}
-    for h in headers:
-        column[h] = []
-    for row in reader:
-        for h, v in zip(headers, row):
-            column[h].append(v)
-            
-testdata = np.genfromtxt ('dataset-har-PUC-Rio-ugulino.csv', delimiter=";")
-testdata = np.delete(testdata, (0), axis=0)
-X = testdata[:,[6,7,8,9,10,11,12,13,14,15,16,17]]
-
-
-#Data pre-processing
-##Encode output variable  
-from sklearn import preprocessing
-le = preprocessing.LabelEncoder()
-le.fit(['sittingdown', 'standingup', 'standing', 'walking', 'sitting'])
-#print(list(le.classes_))
-y = []
-y = le.transform(column['class'])
-normalized_X = preprocessing.normalize(X) 
-
-
-#Evaluate model
-from sklearn.svm import SVC
-from sklearn.model_selection import KFold
-from sklearn.metrics import confusion_matrix
-
-kfold = KFold(n_splits=10, shuffle=True)
-
-fold_index = 0
-for train, test in kfold.split(normalized_X):
-    svm_model_linear = SVC(kernel = 'linear', C = 1).fit(normalized_X[train], y[train])
-    svm_predictions = svm_model_linear.predict(normalized_X[test])
-    accuracy = svm_model_linear.score(normalized_X[test], y[test])
-    cm = confusion_matrix(y[test], svm_predictions)
-    print('In the %i fold, the classification accuracy is %f' %(fold_index, accuracy))
-    print('And the confusion matrix is: ')
-    print(cm)
-    fold_index +=1
-    
