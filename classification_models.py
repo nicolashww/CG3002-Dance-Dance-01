17db1f1289543fffe57c63532c1e7a37e09aa520
Jun Hao <e0012616@u.nus.edu>
2017-09-18 23:27:31 +0800

    Hidden SVN, Rename files

--- classification_models.py
+++ classification_models.py
@@ -11,7 +11,7 @@ tfulls = time.time()
 #Sort data into columns -> For some reason can't extract class column using the better(next) method
 import csv
 import numpy as np 
-with open('dataset-har-PUC-Rio-ugulino.csv') as csvfile:
+with open('dataset_FULL.csv') as csvfile:
     reader=csv.reader(csvfile,delimiter=';')
     headers = next(reader)
     column = {}
@@ -22,7 +22,7 @@ with open('dataset-har-PUC-Rio-ugulino.csv') as csvfile:
             column[h].append(v)
 
 #Sort data into a whole array and extract necessary data
-testdata = np.genfromtxt ('dataset-har-PUC-Rio-ugulino.csv', delimiter=";")
+testdata = np.genfromtxt ('dataset_FULL.csv', delimiter=";")
 testdata = np.delete(testdata, (0), axis=0)
 X = testdata[:,[6,7,8,9,10,11,12,13,14,15,16,17]]
 
@@ -42,7 +42,7 @@ normalized_X = preprocessing.normalize(X)
 
 
 #Evaluate model
-from sklearn.svm import SVC
+#from sklearn.svm import SVC
 from sklearn.neural_network import MLPClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import AdaBoostClassifier
@@ -53,30 +53,30 @@ from sklearn.metrics import confusion_matrix
 n_splits_val = 10
 kfold = KFold(n_splits=n_splits_val, shuffle=True)
 
-#SVM
-print('Support Vector Machines')
-print(' ')
-tsvms = time.time()
-fold_index = 0
-avg_accuracy_svm = 0
-for train, test in kfold.split(normalized_X):
-    tparts = time.time()
-    #SVC vs SVM
-    #LinearSVC is not linear SVM. If you scale it up too much - it will also fail, as now tolerance and number of iterations are crucial. Do not use LinearSVC.
-    svm_model_linear = SVC(kernel = 'linear', C = 1).fit(normalized_X[train], y[train])
-    svm_predictions = svm_model_linear.predict(normalized_X[test])
-    accuracy = svm_model_linear.score(normalized_X[test], y[test])
-    cm = confusion_matrix(y[test], svm_predictions)
-    tparte = time.time()
-    avg_accuracy_svm += accuracy
-    print('In the %i fold, the classification accuracy is %f' %(fold_index, accuracy))
-    print('And the confusion matrix is: ')
-    print(cm)
-    print('This fold took %f seconds' %(tparte-tparts))
-    fold_index +=1
-    print(' ')
-avg_accuracy_svm /= n_splits_val
-tsvme = time.time()
+##SVM
+#print('Support Vector Machines')
+#print(' ')
+#tsvms = time.time()
+#fold_index = 0
+#avg_accuracy_svm = 0
+#for train, test in kfold.split(normalized_X):
+#    tparts = time.time()
+#    #SVC vs SVM
+#    #LinearSVC is not linear SVM. If you scale it up too much - it will also fail, as now tolerance and number of iterations are crucial. Do not use LinearSVC.
+#    svm_model_linear = SVC(kernel = 'linear', C = 1).fit(normalized_X[train], y[train])
+#    svm_predictions = svm_model_linear.predict(normalized_X[test])
+#    accuracy = svm_model_linear.score(normalized_X[test], y[test])
+#    cm = confusion_matrix(y[test], svm_predictions)
+#    tparte = time.time()
+#    avg_accuracy_svm += accuracy
+#    print('In the %i fold, the classification accuracy is %f' %(fold_index, accuracy))
+#    print('And the confusion matrix is: ')
+#    print(cm)
+#    print('This fold took %f seconds' %(tparte-tparts))
+#    fold_index +=1
+#    print(' ')
+#avg_accuracy_svm /= n_splits_val
+#tsvme = time.time()
 
 #NN
 print(' ')
@@ -177,9 +177,9 @@ print(' ')
 
 #Results
 print('Results:')
-print(' ')
-print('Support Vector Machines took %f seconds' %(tsvme-tsvms))
-print('with an average accuracy of %f%%' %(avg_accuracy_svm*100))
+#print(' ')
+#print('Support Vector Machines took %f seconds' %(tsvme-tsvms))
+#print('with an average accuracy of %f%%' %(avg_accuracy_svm*100))
 print(' ')
 print('Neural network model took %f seconds' %(tnne-tnns))
 print('with an average accuracy of %f%%' %(avg_accuracy_nn*100))
@@ -197,16 +197,16 @@ print(' ')
 
 
 #Extract some random values
-sample = normalized_X[[100,300,500,700,900],:]
-sampletruth = y[[100,300,500,700,900],]
+sample = normalized_X[[100,57759,66113,116595,134393],:]
+sampletruth = y[[100,57759,66113,116595,134393],]
 
 print('Sample data(normalized):')
 print(sample)
 print(' ')
 print('Correct result:')
 print(sampletruth)
-print('SVM predicted result:')
-print(svm_model_linear.predict(sample))
+#print('SVM predicted result:')
+#print(svm_model_linear.predict(sample))
 print('NN predicted result:')
 print(nn_model.predict(sample))
 print('KNN predicted result:')
