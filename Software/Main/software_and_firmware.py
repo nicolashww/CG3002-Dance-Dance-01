f0d00868c2188aa80ab9eaffea0fcf229208e8d2
Jun Hao <e0012616@u.nus.edu>
2017-10-29 09:33:43 +0800

    Code updates

--- Software/Main/software_and_firmware.py
+++ Software/Main/software_and_firmware.py
@@ -5,36 +5,72 @@ Created on Fri Oct 27 16:09:01 2017
 @author: Jun Hao
 """
 
+#Set print command to print to file
+import sys
+sys.stdout = open("Output.txt", "w")
+
+#Print current time on computer
+from datetime import datetime
+print (str(datetime.now()))
+
+#Implement simple timer
+import time
+current_milli_time = lambda: int(round(time.time() * 1000))
+#startTime = current_milli_time()
+
 import pandas as pd
 import numpy as np
-#import csv
-#from sklearn import preprocessing
+import csv
+from sklearn import preprocessing
 import serial
 
-#Declare empty dataframe
-fullDF = pd.DataFrame(columns = ['accID', 'x0', 'y0', 'z0', 'x1', 'y1', 'z1', 'x2', 'y2', 'z2', 'x3', 'y3', 'z3', 'pwr'])
 
-ser = serial.Serial("/dev/ttyACM3", baudrate=9600, timeout=3.0)
+# Declarations
 flag = 1
+ignoreloopcount = 0
 loopcount = 0
+newAccID = 0
+oldAccID = 0
+oldTime = current_milli_time()
+newTime = current_milli_time()
+# Declare column headers
+cols = ['accID', 'x0', 'y0', 'z0', 'x1', 'y1', 'z1', 'x2', 'y2', 'z2', 'x3', 'y3', 'z3', 'pwr', 'hash']
+fullDF = pd.DataFrame(columns=cols)
+
+# Initialize serial
+ser = serial.Serial("/dev/ttyACM3", baudrate=9600, timeout=3.0)
 print("Raspberry Pi Ready")
 
-while (loopcount<10):
+# Ignore first 20 readings
+print("Ignoring starting readings")
+while (ignoreloopcount < 20):
     while flag == 1:
         ser.write("\r\nH")
-        print("H sent")
+        print("H sent, awaiting response...")
         response = ser.read()
         if response == 'A':
-            print("Handshake complete")
+            print("Response verified, handshake complete")
+            print("Begin reading:")
             flag = 0
             ser.write("\r\nA")
     message = ser.readline()
-    messagenp = np.fromstring(message, dtype=int, sep=",")
-    messagepd = pd.DataFrame(data=messagenp.reshape(-1, len(messagenp)), index=['1'], columns=['accID', 'x0', 'y0', 'z0', 'x1', 'y1', 'z1', 'x2', 'y2', 'z2', 'x3', 'y3', 'z3', 'pwr'])
-    print(messagepd)
-    fullDF = fullDF.append(messagepd, ignore_index = True)
-    loppcount = loopcount + 1
+    print(message)
+    ignoreloopcount += 1
     ser.write("\r\nA")
 
+# Read (Main Loop)
+while (loopcount < 50):
+    newTime = current_milli_time()
+    message = ser.readline()
+    newAccID = int(message.split(',')[0])
+    if ((newAccID > oldAccID) & (newTime >= oldTime)):
+        messagenp = np.fromstring(message, dtype=int, sep=",")
+        messagepd = pd.DataFrame(data=messagenp.reshape(-1, len(messagenp)), index=['1'], columns=cols)
+        print(messagepd)
+        fullDF = fullDF.append(messagepd, ignore_index = True)
+        loopcount += 1
+        oldAccID = newAccID
+        oldTime = newTime + 20 #20ms => 50Hz
+        ser.write("\r\nA")
+
 print(fullDF)
-    
