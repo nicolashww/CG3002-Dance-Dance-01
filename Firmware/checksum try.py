143a4e8f9cbdbe1f3b07fbcf4aee6fe5651b1755
Jun Hao <e0012616@u.nus.edu>
2017-10-29 14:00:22 +0800

    Added checksum test file

---
+++ Firmware/checksum try.py
@@ -0,0 +1,23 @@
+message = "test"
+byteMessage = message.encode('utf-8')
+print(byteMessage)
+
+i = 0
+checkSum = 0
+
+while i < (len(byteMessage)-1):
+    checkSum ^= byteMessage[i]
+    print(i, ": ")
+    print(message[i], "\n")
+    i += 1
+
+print(checkSum)
+print(chr(checkSum))
+print(hex(checkSum))
+
+if chr(checkSum) == message[len(message)-1]:
+    print('Correct')
+    #Store data into buffer
+else:
+    print('Not Correct')
+    #Send request for resend of data to Arduino
